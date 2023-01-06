5a6989096621f689a3a4a7803951c781ddbd5376
Manu Mathew <a0393608@uda0393608>
2023-01-06 20:44:51 +0530

    requirements have been updated

--- setup_for_modelmaker.sh
+++ setup_for_modelmaker.sh
@@ -4,3 +4,7 @@ sudo apt install cmake>=3.22
 
 # Dependencies for building pillow-simd
 pip3 install -r requirements_modelmaker.txt
+
+# there as issue with installing pillow-simd through requirements - force it here
+pip uninstall pillow
+pip install --no-input -U --force-reinstall pillow-simd
